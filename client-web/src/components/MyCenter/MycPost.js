360b10dcc917f99ab2a51b0ab24304914868e9cc
boleixiongdi <bl.jiwenlei@gmail.com>
2016-08-09 08:35:37 +0800

    perfect post function

--- client-web/src/components/MyCenter/MycPost.js
+++ client-web/src/components/MyCenter/MycPost.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
+import { Link, IndexLink, browserHistory  } from 'react-router'
 import Simditor from 'simditor'
 import $ from 'jquery'
 
@@ -17,7 +18,7 @@ export default class MycPost extends Component {
         'ol', 'ul', 'blockquote', 'code', 'table', 'link', 'image', 'indent', 'outdent', 'alignment', 'hr']
     });
     this.editor.on("valuechanged", (e, src) => {
-      console.info("this.props.fields",this.props.fields);
+      //监听到编辑器的变化，获取到内容保存到数据库
       console.info("this.editor.getValue()",this.editor.getValue());
     });
   }
@@ -33,9 +34,19 @@ export default class MycPost extends Component {
     return (
       <div className="ant-layout-aside">
         <h3>写文章</h3>
+        <form className="form-horizontal" >
+          <div className="form-group form-group-sm" style={{marginLeft: 0}} >
+            <label style={{textAlign: 'center',color: '#696969'}} className="col-sm-2 control-label search-btn" >标题</label>
+            <div className="col-sm-10">
+              <input className="form-control" type="text" placeholder="文章标题" />
+            </div>
+          </div>
+        </form>
         <div>
-          <div>写文章界面</div>
           <textarea className="form-control" ref='textarea' rows="50" />
+        </div>
+        <div className="align-ct">
+          <button className="bl-btn">保存</button>
         </div>
       </div>
     )
